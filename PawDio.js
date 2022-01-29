97796493d71eadcb6f4f21c3812f41537365adc0
Marcelino <93845376+DreamxProtocol@users.noreply.github.com>
2022-01-29 15:18:34 -0800

    JavaScript and name change
    added JavaScript that incorporates sound with keys. Name change to some files.

--- PawDio.js
+++ PawDio.js
@@ -7,50 +7,70 @@ const kickElectro = document.querySelector("#kickElectro");
 
 const padControllers =  {
     65: {pressed: false},
-    83: {pressed: false} 
+    83: {pressed: false},
+    68: {pressed: false},
+    66: {pressed: false},
+    78: {pressed: false},
+    77: {pressed: false}
 } 
+
+function playSamples() {
+    if (padControllers[65].pressed == true){
+        tomRototom.setAttribute("loop", "")
+        tomRototom.play();
+    }
+
+    if (padControllers[83].pressed == true){
+        snareModular.setAttribute("loop", "")
+        snareModular.play();
+    }
+
+    if (padControllers[68].pressed == true){
+        clapAnalog.setAttribute("loop", "")
+        clapAnalog.play(); 
+    }
+
+    if (padControllers[66].pressed == true){
+        tomAnalog.setAttribute("loop", "")
+        tomAnalog.play();
+    }
+
+    if (padControllers[78].pressed == true){
+        kickElectro.setAttribute("loop", "")
+        kickElectro.play();
+    }
+
+    if (padControllers[77].pressed == true){
+        kickZapper.setAttribute("loop", "")
+        kickZapper.play();  
+    }   
+}
 
 document.addEventListener("keydown", (e) => {
 
     if (padControllers[e.keyCode]){
         padControllers[e.keyCode].pressed = true;
-        console.log(padControllers);
     }
-    playSamples();
-    
+
+    playSamples();  
 })
 
 document.addEventListener("keyup", (e) => {
 
     if (padControllers[e.keyCode]){
         padControllers[e.keyCode].pressed = false;
-        clapAnalog.removeAttribute("loop")
+        clapAnalog.removeAttribute("loop");
         kickZapper.removeAttribute("loop");
-        console.log(padControllers);
+        tomAnalog.removeAttribute("loop");
+        tomRototom.removeAttribute("loop");
+        snareModular.removeAttribute("loop");
+        kickElectro.removeAttribute("loop");
     }
 })
 
 
 
-function playSamples() {
-if (padControllers[65].pressed == true){
-    clapAnalog.setAttribute("loop", "")
-    clapAnalog.play();
-} else {
-    
-    
 
-}
-
-if (padControllers[83].pressed == true){
-
-    kickZapper.play();
-    kickZapper.setAttribute("loop", "")
-
-}
-
-
-}
 
 
 
